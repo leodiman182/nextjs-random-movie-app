e63e5fd50642ec7b03a7e5dc55fb3d0dd78c4f1d
Leonardo Martins <leonardo.martins@cotefacil.com>
2023-06-14 14:49:50 -0300

    Botões animados

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,5 @@
+import { RiMovie2Fill } from 'react-icons/ri';
+
+export default function Loading() {
+  return <RiMovie2Fill />;
+}
--- src/components/MainCard/index.jsx
+++ src/components/MainCard/index.jsx
@@ -23,13 +23,13 @@ export default function MainCard() {
       <h3 className="text my-[15px] text-[24px] lg:text-[30px]">
         Use the app to get a random movie
       </h3>
-      <button className="text bg-primary rounded-md py-[10px] px-[20px] lg:px-[40px] text-white lg:text-[28px] my-[50px] opacity-80 hover:opacity-100 duration-150">
+      <button className="text bg-primary rounded-md py-[10px] px-[20px] lg:px-[40px] text-white lg:text-[28px] my-[50px] opacity-80 hover:opacity-100 hover:bg-white hover:text-primary hover:scale-110 duration-150">
         Surprise me!
       </button>
       <div className="flex flex-col w-full">
         {buttons.map((el, index) => (
           <button
-            className="lg:w-3/4 bg-secondary my-[8px] lg:my-[12px] py-[6px] rounded-md text-black lg:text-[20px] lg:mx-auto lg:opacity-80 hover:opacity-100 duration-150"
+            className="lg:w-3/4 bg-black border-2 border-secondary my-[8px] lg:my-[12px] py-[6px] rounded-md text-secondary lg:text-[20px] lg:mx-auto lg:opacity-70 hover:opacity-100 hover:bg-secondary hover:text-black duration-150"
             key={index}
             onClick={el.func}
           >
